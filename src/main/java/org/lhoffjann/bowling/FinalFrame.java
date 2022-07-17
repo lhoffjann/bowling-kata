4608df64134e6e25a59cd0817cfd4d3af5e6053a
lennart <hoff.lenn@gmail.com>
2022-07-17 12:47:26 +0200

    added subclass of Frame and defined the frame creation

---
+++ src/main/java/org/lhoffjann/bowling/FinalFrame.java
@@ -0,0 +1,4 @@
+package org.lhoffjann.bowling;
+
+public class FinalFrame extends Frame {
+}
--- src/main/java/org/lhoffjann/bowling/Game.java
+++ src/main/java/org/lhoffjann/bowling/Game.java
@@ -5,6 +5,17 @@ public class Game {
     public Frame[] framesOfGame;
 
     public Game(){
+        framesOfGame = new Frame[TOTAL_NUMBER_OF_FRAMES];
+        for(int i = 1; i <= TOTAL_NUMBER_OF_FRAMES; i++){
+        if (i < TOTAL_NUMBER_OF_FRAMES){
+            framesOfGame[i] = new Frame();
+
+        } else if (i == TOTAL_NUMBER_OF_FRAMES) {
+            // needs to be a different frame because it is the final frame
+            framesOfGame[i] = new FinalFrame();
+
+        }
+        }
 
 
     }
