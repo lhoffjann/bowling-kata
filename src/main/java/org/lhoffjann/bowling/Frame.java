8b52a66cfd2b5344ba7f754fafec26abd146b975
lennart <hoff.lenn@gmail.com>
2022-07-17 12:33:18 +0200

    defined the relation between classes

--- src/main/java/org/lhoffjann/bowling/Frame.java
+++ src/main/java/org/lhoffjann/bowling/Frame.java
@@ -1,6 +1,14 @@
 package org.lhoffjann.bowling;
 
 public class Frame {
-    public Roll[] RollsOfFrame;
+    public Roll[] rollsOfFrame;
+    public int score;
+    public boolean isDuplicated;
+
+    public Frame(){
+        rollsOfFrame = new Roll[2];
+        rollsOfFrame[0] = new Roll();
+        rollsOfFrame[1] = new Roll();
+    }
 
 }
--- src/main/java/org/lhoffjann/bowling/Game.java
+++ src/main/java/org/lhoffjann/bowling/Game.java
@@ -1,3 +1,11 @@
 package org.lhoffjann.bowling;
+
 public class Game {
+    public final int TOTAL_NUMBER_OF_FRAMES = 10;
+    public Frame[] framesOfGame;
+
+    public Game(){
+
+
+    }
 }
