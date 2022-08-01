ad2bf0798d178242ec763b1623181ff2f3af4773
lennart <hoff.lenn@gmail.com>
2022-08-01 18:10:49 +0200

    refactored Frame

--- src/main/java/org/lhoffjann/bowling/Frame.java
+++ src/main/java/org/lhoffjann/bowling/Frame.java
@@ -3,26 +3,33 @@ package org.lhoffjann.bowling;
 import java.util.ArrayList;
 
 public class Frame {
-    public ArrayList<Roll> rolls;
+    public final ArrayList<Roll> rolls;
 
     public Frame() {
         this.rolls = new ArrayList<>();
     }
 
-    public int getScoreOfRoll(int roll) { return rolls.get(roll).score(); }
+    public int getScoreOfRoll(int roll) {
+        return rolls.get(roll).score();
+    }
 
-    public void roll(int PinsDown){
-        this.rolls.add(new Roll(PinsDown));
+    public void addRoll(int PinsDown){
+        rolls.add(new Roll(PinsDown));
     }
 
     public int getScore() {
         int scoreOfThisFrame = 0;
-        for(Roll roll : this.rolls){
+        for(Roll roll : rolls){
             scoreOfThisFrame += roll.score();
         }
         return scoreOfThisFrame;
     }
 
-    public boolean isStrike() { return rolls.get(0).score() == 10; }
-    public boolean isSpare() { return this.getScore() == 10 && !this.isStrike(); }
+    public boolean isStrike() {
+        return !rolls.isEmpty() && rolls.get(0).score() == 10;
     }
+
+    public boolean isSpare() {
+        return this.getScore() == 10 && !this.isStrike();
+    }
+}
