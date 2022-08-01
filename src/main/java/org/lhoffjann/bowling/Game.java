58c9ad4a56f6a00d13f2b5631f5462e109c41c80
lennart <hoff.lenn@gmail.com>
2022-08-01 18:18:47 +0200

    refactored Game

--- src/main/java/org/lhoffjann/bowling/Game.java
+++ src/main/java/org/lhoffjann/bowling/Game.java
@@ -15,15 +15,18 @@ public class Game {
         for(int i = 0; i < numberOfFrames; i++) {
             Frame frame = new Frame();
             if (i == numberOfFrames-1 & cursor == PinsDown.length - 3) {
-                frame.roll(PinsDown[cursor]);
-                frame.roll(PinsDown[cursor+ 1]);
-                frame.roll(PinsDown[cursor+ 2]);
+                // last frame with 3 rolls
+                frame.addRoll(PinsDown[cursor]);
+                frame.addRoll(PinsDown[cursor+ 1]);
+                frame.addRoll(PinsDown[cursor+ 2]);
             } else if (PinsDown[cursor] == 10) {
-                frame.roll(PinsDown[cursor]);
+                //strike
+                frame.addRoll(PinsDown[cursor]);
                 cursor += 1;
             } else {
-               frame.roll(PinsDown[cursor]);
-               frame.roll(PinsDown[cursor+1]);
+                //regular two-roll frame
+               frame.addRoll(PinsDown[cursor]);
+               frame.addRoll(PinsDown[cursor+1]);
                cursor += 2;
            }
            frames.add(frame);
@@ -38,22 +41,35 @@ public class Game {
 
         for (int i = 0; i < frames.size(); i++) {
             currentFrame = frames.get(i);
+            // setting nextFrame and making sure it isnt outofRange
             if (i < frames.size() - 1){
-                nextFrame = frames.get(i+1);
+                nextFrame = frames.get(i + 1);
             }
+            // setting nextnextFrame and making sure it isnt outofRange
             if(i < frames.size() - 2){
+
                 nextNextFrame = frames.get(i + 2);
             }
+            // adding last throw to the score
             if(i == frames.size() - 1) {
+
                 score += currentFrame.getScore();
+
+            // Edgecase of the frame before last frame is a strike
             }else if(i == frames.size() - 2 & currentFrame.isStrike()){
+
                 score += currentFrame.getScore() + nextFrame.getScoreOfRoll(0) + nextFrame.getScoreOfRoll(1);
             } else if (currentFrame.isStrike() & nextFrame.isStrike()){
+
                 score += currentFrame.getScore() + nextFrame.getScore() + nextNextFrame.getScoreOfRoll(0);
+
             } else if (currentFrame.isStrike()) {
+
                 score += currentFrame.getScore() + nextFrame.getScore();
+
             } else if (currentFrame.isSpare()){
                 score += currentFrame.getScore() + nextFrame.getScoreOfRoll(0);
+            // normal frame
             }else {
                 score += currentFrame.getScore();
             }
